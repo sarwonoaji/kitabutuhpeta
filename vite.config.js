e4c9e2372c2ccdebbd6cb45a68bb930d7610b013
Aji <sarwonoaji27@gmail.com>
2026-04-22 13:03:12 +0700

    grafik tamu dan persyaratan

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        port: 5174,
+        hmr: {
+            host: '10.110.31.160',
+            port: 5174,
+        },
+    },
 });
